4f1d886ebb3c987785c4fdcfe14845845622a1b6
rohit-karthik <rohit.karthik200@gmail.com>
2021-09-20 15:56:38 +0000

    finish sound, add min to stopwatch

--- src/components/Stopwatch.svelte
+++ src/components/Stopwatch.svelte
@@ -1,6 +1,6 @@
 <script>
     let count = 0;
-
+    let countSec = 0;
     let started = false;
 
     let interval;
@@ -11,30 +11,56 @@
     class="flex flex-col h-full justify-center items-center bg-white dark:bg-gray-800"
 >
     <p class="text-blue-400 text-5xl mb-6">Stopwatch</p>
-    <p
-        class="
-            mt-2
-            flex
-            w-60
-            text-center
-            text-blue-400
-            ml-2
-            mb-6
-            h-20
-            border-blue-400
-            border-2
-            rounded-lg
-            items-center
-            justify-center
-            text-3xl
-            transition
-            hover:border-none
-            hover:bg-blue-400
-            hover:text-white
-        "
-    >
-        {count} {count == 1 ? 'second' : 'seconds'}
-    </p>
+    <div class="flex">
+        <p
+            class="
+                mt-2
+                flex
+                w-32
+                text-center
+                text-blue-400
+                ml-2
+                mb-6
+                h-20
+                border-blue-400
+                border-2
+                rounded-lg
+                items-center
+                justify-center
+                text-3xl
+                transition
+                hover:border-none
+                hover:bg-blue-400
+                hover:text-white
+            "
+        >
+            {count} min
+        </p>
+        <p
+            class="
+                mt-2
+                flex
+                w-32
+                text-center
+                text-blue-400
+                ml-2
+                mb-6
+                h-20
+                border-blue-400
+                border-2
+                rounded-lg
+                items-center
+                justify-center
+                text-3xl
+                transition
+                hover:border-none
+                hover:bg-blue-400
+                hover:text-white
+            "
+        >
+            {countSec} sec
+        </p>
+    </div>
 
     <div class="flex w-60">
         <button
@@ -50,10 +76,14 @@
             class="bg-green-400 text-white p-4 mx-2 rounded-lg active:bg-green-600"
             on:click={() => {
                 if (!started) {
-                    interval = setInterval(() => {
-                        count += 1;
-                    }, 1000);
                     started = true;
+                    interval = setInterval(() => {
+                        countSec += 1;
+                        if (countSec == 60) {
+                            count += 1
+                            countSec = 0
+                        }
+                    }, 1000);
                 }
             }}>Start</button
         >
@@ -61,6 +91,7 @@
             class="bg-red-600 text-white p-4 ml-2 rounded-lg active:bg-red-800"
             on:click={() => {
                 count = 0;
+                countSec = 0;
             }}>Reset</button
         >
     </div>
